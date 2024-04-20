762d79881d248518a6c6c45e529420a0a1e6d190
Paola Valdivia <p.llerenavaldivia@criteo.com>
2024-04-20 16:58:12 +0200

    🧹 clean comments

--- load-context.ts
+++ load-context.ts
@@ -1,29 +1,9 @@
 import { type PlatformProxy } from "wrangler";
-// import {connection} from "./db/client";
-// import {AppLoadContext} from "@remix-run/cloudflare";
-
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
-    // db: Awaited<ReturnType<typeof connection>>
   }
 }
-
-// type GetLoadContext = (args: {
-//   request: Request
-//   context: {
-//     cloudflare: Cloudflare
-//   } // load context _before_ augmentation
-// }) => Promise<AppLoadContext>
-//
-// export const getLoadContext: GetLoadContext = async ({ context }) => {
-//   return {
-//     ...context,
-//     db: await connection(
-//         context.cloudflare.env.DB,
-//     ),
-//   }
-// }
