3cd67b3c60d6432b2b9a277316139a9aa572fe07
Paola Valdivia <p.llerenavaldivia@criteo.com>
2024-05-13 16:20:25 +0200

    💄 guests table

--- app/routes/guests.tsx
+++ app/routes/guests.tsx
@@ -1,7 +1,15 @@
-import { json, LoaderFunctionArgs } from "@remix-run/cloudflare";
+import {
+  json,
+  type LinksFunction,
+  LoaderFunctionArgs,
+} from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 import { repository } from "../../src/repository/repository";
 import { authenticator } from "~/services/auth.server";
+
+import styles from "~/styles/guests.css?url";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const loader = async ({ request, context }: LoaderFunctionArgs) => {
   await authenticator.isAuthenticated(request, {
@@ -13,16 +21,44 @@ export const loader = async ({ request, context }: LoaderFunctionArgs) => {
 
 export default function Guest() {
   const { guests } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <h1>Guest</h1>
-      <ul>
-        {guests.map((guest) => (
-          <li key={guest.id}>
-            {guest.name} {guest.lastName}
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Attend</th>
+            <th>Plus One</th>
+            <th>Plus One Name</th>
+            <th>Plus One Last Name</th>
+            <th>Kids</th>
+            <th>Comments</th>
+          </tr>
+        </thead>
+        <tbody>
+          {guests.map((guest) => (
+            <tr key={guest.id}>
+              <td>{guest.id}</td>
+              <td>{guest.name}</td>
+              <td>{guest.lastName}</td>
+              <td>{guest.email}</td>
+              <td>{guest.phone}</td>
+              <td>{guest.attend ? "Yes" : "No"}</td>
+              <td>{guest.plusOne ? "Yes" : "No"}</td>
+              <td>{guest.plusOneName}</td>
+              <td>{guest.plusOneLastName}</td>
+              <td>{guest.kids}</td>
+              <td>{guest.comments}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
