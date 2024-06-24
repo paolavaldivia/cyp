715aee6f254d02ec8a25f575048435096ef0b93a
Paola Valdivia <p.llerenavaldivia@criteo.com>
2024-06-24 17:06:51 +0200

    ✨ add address

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,8 +4,10 @@ import { ClientOnly } from "remix-utils/client-only";
 import styles from "~/styles/home.css?url";
 import divider from "~/images/divider.svg";
 import { MainPhoto } from "~/components/mainPhoto";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { TitleClient } from "~/components/title.client";
+import { ColorPaletteClient } from "~/components/colorPalette.client";
+import { paletteColors } from "../../src/utils/paletteColors";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
@@ -48,15 +50,23 @@ export default function Index() {
             </p>
             <p>
               La ceremonia se llevará a cabo el <b> 24 de Agosto</b> en Arequipa
-              (dirección exacta a ser confirmada).
+              en "El Rosario Salón de Eventos".
             </p>
             <p>Únete a nosotros para celebrar este día tan especial.</p>
           </div>
           <img src={divider} alt="" height={20} />
-          <Link className="button" to="/rsvp">
-            RSVP
-          </Link>
-          <p> antes del 2 de junio</p>
+          <div className="home-main-extra">
+            <div>
+              <div className="home-main-extra-title">Dirección:</div>
+              Sector Bajo Cural lote 425 Lateral 7 - 3 Uchumayo.
+            </div>
+            <div>
+              <div className="home-main-extra-title">Paleta de la boda:</div>
+              <ClientOnly fallback={<div />}>
+                {() => <ColorPaletteClient paletteColors={paletteColors} />}
+              </ClientOnly>
+            </div>
+          </div>
         </div>
       </div>
       {/*<CardRound orientation="left" className="home-story">*/}
